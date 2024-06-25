90bf1d12f601046857619547b2bf8e9bcd0c1742
Andrei <aakoval01@gmail.com>
2024-06-25 16:27:22 +0300

    Initial Commit

---
+++ ServiceHandsTestApp_KovalenkoAA/ServiceHandsTestApp_KovalenkoAAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceHandsTestApp_KovalenkoAAApp.swift
+//  ServiceHandsTestApp_KovalenkoAA
+//
+//  Created by Андрей Коваленко on 25.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ServiceHandsTestApp_KovalenkoAAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
